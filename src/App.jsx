a4a23999df73fec3cc7d8012432035369f0d59b7
RagaviRajasekaran <143269843+RajRagavi@users.noreply.github.com>
2023-10-31 02:24:42 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ParentComponent from './components/ParentComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>User Management App</h1>
+      <ParentComponent />
+    </div>
+  );
+}
+
+export default App;
